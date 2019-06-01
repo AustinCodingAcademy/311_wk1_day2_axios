d151c9d503551a4b7a38cf22c74e9727c44b4e30
Matt Viteri <mxviteri@gmail.com>
2019-06-01 12:04:30 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// import axios here
+
+// Please note that it is normally not considered best practice to commit 
+// api keys to github as it presents a security risk. It is done here only 
+// for practice purposes as we are sharing the same account
+const api_key = 'd771b19ef336ed8381def3a60b574464'
+
+const discoverMovie = () => {
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
+  // code here
+}
+
+const getMovieById = (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
+  // code here
+}
+
+const getMovieByIdFailure = () => {
+  const fakeId = 1 // FAKE ID HERE
+  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
+  // code here
+}
+
+
+
+module.exports = {
+  discoverMovie,
+  getMovieById,
+  getMovieByIdFailure
+}

dd6ca3fa82dc9c5ddc3c4791eb9ac479ea037980
State Tyler <statetyler@State-Tylers-MacBook-Pro.local>
2019-08-29 19:57:06 -0500

    Final

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 // import axios here
-
+let axios = require('axios');
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
 const api_key = 'd771b19ef336ed8381def3a60b574464'
 
 const discoverMovie = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
-  // code here
+	const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
+	// code here
+	return axios.get( url )
+		.then( (response) => ( response ) );
 }
 
 const getMovieById = (id) => {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
+	const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
+	// code here
+	return axios.get( url )
+		.then( (response) => {
+			return response.data
+		});
 }
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
-  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
-  // code here
+	const fakeId = 5783 // FAKE ID HERE
+	const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
+	// code here
+	return axios.get( url )
+		.then((response) => {
+			return response.status
+		}).catch((error) => {
+			return error.response.status
+		})
 }
 
 
 
 module.exports = {
-  discoverMovie,
-  getMovieById,
-  getMovieByIdFailure
+	discoverMovie,
+	getMovieById,
+	getMovieByIdFailure
 }

09d9a7d9d22251172709c875f8e770ff26fbb045
nae deshazer <naedeshazer@naes-MacBook-Pro.local>
2019-08-29 19:57:24 -0500

    Iseanee Axios

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 // import axios here
-
-// Please note that it is normally not considered best practice to commit 
-// api keys to github as it presents a security risk. It is done here only 
+let axios = require("axios");
+// Please note that it is normally not considered best practice to commit
+// api keys to github as it presents a security risk. It is done here only
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+const api_key = "d771b19ef336ed8381def3a60b574464";
 
 const discoverMovie = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
-  // code here
-}
+	const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`;
+	// code here
+	//create an http request message with a certain path and method//
+	return axios.get(url).then(function(response) {
+		// handle success
+		return response;
+	});
+};
 
-const getMovieById = (id) => {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
+const getMovieById = id => {
+	const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`;
+	//create an http request message with a certain path and method//
+  return axios.get(url)
+    .then(function(result) {
+		// handle success
+		return (result.data);
+})
 }
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
-  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
+	const fakeId = 5783; // FAKE ID HERE
+	const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`;
   // code here
-}
-
-
+  return axios.get(url)
+  .then(response => response.status)
+  .catch(err => err.response.status);
+};
 
 module.exports = {
-  discoverMovie,
-  getMovieById,
-  getMovieByIdFailure
-}
+	discoverMovie,
+	getMovieById,
+	getMovieByIdFailure
+};

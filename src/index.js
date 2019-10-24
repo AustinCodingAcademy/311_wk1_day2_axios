12d8607fa12628a5b59fd8405b41bf58cfadf8f8
amberstyers <amberstyers@gmail.com>
2019-10-23 20:09:58 -0500

    axios practice done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import axios here
+const axios = require ('axios')
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
@@ -8,18 +9,37 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  return axios.get(url)
+  .then(response => {
+    // handle success
+    console.log(response);
+    return response;
+  })
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
+  return axios.get(url)
+  .then(result => {
+    return result.data;
+  })
 }
+    
+
 
 const getMovieByIdFailure = () => {
   const fakeId = 1 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
-  // code here
-}
+  return axios.get(url)
+  .then( (status) => {
+    return status;
+  })
+  .catch((error) => {
+    return error.response.status
+  })
+
+  }
+    
 
 
 

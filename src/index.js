5708da9e880d3d683e59bbb86ad9fa900c95d5fa
Alex Munoz <anmunoz24@gmail.com>
2019-10-23 18:10:37 -0700

    3 tests passing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import axios here
+const axios = require('axios')
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
@@ -8,17 +9,29 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  //constructing an http message
+  return axios.get(url)
+  .then ((response) => {
+    //handle success 
+    return response
+  })
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  return axios.get(url) 
+  .then((response => response.data))
+
 }
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  return axios.get(url) 
+  .then((response => response.status)) 
+  .catch ((error => error.response.status))    
 }
 
 

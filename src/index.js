5da8b5898e8cc1f308c61f2b00f12608095e0114
tonezone108 <tonezone108@gmail.com>
2019-11-07 21:15:07 -0600

    axios practice

--- src/index.js
+++ src/index.js
@@ -1,30 +1,55 @@
 // import axios here
+const axios = require("axios");
 
-// Please note that it is normally not considered best practice to commit 
-// api keys to github as it presents a security risk. It is done here only 
+// Please note that it is normally not considered best practice to commit
+// api keys to github as it presents a security risk. It is done here only
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+const api_key = "d771b19ef336ed8381def3a60b574464";
 
 const discoverMovie = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`;
   // code here
-}
+  return axios(url).then(response => {
+    console.log(response);
+    return response;
+  });
+};
+// discoverMovie();
+const getMovieById = id => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`;
+  // code here
 
-const getMovieById = (id) => {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
-}
+  return axios(url)
+    .then(response => response)
+    .then(result => {
+      console.log(result.data);
+      return result.data;
+    });
+};
+
+// getMovieById(500);
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
-  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
+  const fakeId = 5783; // FAKE ID HERE
+  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`;
   // code here
-}
 
+  return axios(url)
+    .then(response => response)
+    .then(result => {
+      console.log(result.data);
+      return result.data;
+    })
+    .catch(error => {
+      console.log(error);
+      return error.response.status;
+    });
+};
 
+// getMovieByIdFailure();
 
 module.exports = {
   discoverMovie,
   getMovieById,
   getMovieByIdFailure
-}
+};

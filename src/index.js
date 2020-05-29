6c3a101438007caf8cb72b2b22cf0151e326eb3f
evantwidwell <evanski@gmail.com>
2020-05-28 20:49:53 -0500

    adding Axios assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import axios here
-
+const axios = require('axios')
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
@@ -8,18 +8,40 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  
+  return axios.get(url)
+  .then(function(response){
+    console.log(response)
+    console.log("Hellow World")
+  })
 }
+// discoverMovie();
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  axios.get(url)
+  .then(function(response){
+    console.log(response.data)
+    return response.data
+  })
 }
-
+// getMovieById(500)
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  axios.get(url)
+  .then(function(response){
+    console.log(response)
+  
+  })
+  .catch(error =>{
+    console.log(error.response.status)
+    return error.response.status
+  })
 }
+// getMovieByIdFailure()
 
 
 

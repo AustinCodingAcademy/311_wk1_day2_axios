4f7ad1e94d21e92593fd9c64653419504bb304f4
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-05-28 20:38:02 -0500

    passing all test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import axios here
+const axios = require('axios')
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
@@ -8,17 +9,32 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  return axios.get(url)
+  .then(function (response) {
+    return response
+  })
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  return axios.get(url)
+  .then(function(result) {
+    return result.data
+  })
 }
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // replaced 1 with number now there
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  return axios.get(url) 
+  .then(function(response) {
+    return response
+  })
+  .catch(function(error) {
+    return error.response.status
+  })
 }
 
 

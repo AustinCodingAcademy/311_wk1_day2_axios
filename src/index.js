e9eed7fa13baba2c71e8721ab9e20157eccb92f2
TEldridge <skippydrumskins@yahoo.com>
2020-08-11 09:53:38 -0500

    made all tests pass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+const { default: Axios } = require('axios');
+
 // import axios here
+const axios = require('axios').default;
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
@@ -7,18 +10,23 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
+  
+    return axios(url)
+    
   // code here
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  return axios(url).then(results => results.data)
 }
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  return axios(url).then(response => response.status).catch(error => error.response.status)
 }
 
 

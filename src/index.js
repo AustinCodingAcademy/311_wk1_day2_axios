ed85c74247e6d1551e8ca6a6568e0b98f17f48e5
TEldridge <skippydrumskins@yahoo.com>
2020-08-11 10:46:10 -0500

    added .env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-const { default: Axios } = require('axios');
+// const { default: Axios } = require('axios');
 
 // import axios here
 const axios = require('axios').default;
-
+const dotenv = require('dotenv')
+dotenv.config()
+const api_key = process.env.API_KEY
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`

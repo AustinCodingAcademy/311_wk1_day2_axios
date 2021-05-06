700d3e14b8741992d72debcb1b28057e44212888
RacMcT <raccmct@gmail.com>
2021-05-05 20:48:19 -0500

    class work- initial functions done- need to run tests to check

--- src/index.js
+++ src/index.js
@@ -8,17 +8,28 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+
+  // axios(url).then((response) => {
+  //   return(response.data.results)
+  // })
+
+  return axios(url)
+
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  return axios(url).then((result) => result.data)
 }
 
 const getMovieByIdFailure = () => {
   const fakeId = 1 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  return axios(url)
+  .then((result) => result)
+    .catch((error) => error.response.status)
 }
 
 

555dca849aeb966e1230c2929647507a5fd43f90
RacMcT <raccmct@gmail.com>
2021-05-05 20:59:00 -0500

    passed 2 unit test and commented error message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import axios here
-
+const axios = require('axios');
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
@@ -9,11 +9,11 @@ const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
 
-  // axios(url).then((response) => {
-  //   return(response.data.results)
-  // })
+  axios(url).then((response) => {
+    return(response.data.results);
+  })
 
-  return axios(url)
+  //BUG: NOT PASSING TEXT BECAUSE IT IS RETURNING UNDEFINED OR NULL 
 
 }
 

a48c64fb06af0b9dfa1e77c4ff0f116d8418c756
Pena-Martinez <Rocio.PenaMartinez@austintexas.gov>
2021-05-09 20:44:00 -0500

    basic functions for all 3 tests. added return axios to 1st. 1st test passed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
-// import axios here
+const axios = require('axios');
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+const api_key = 'd771b19ef336ed8381def3a60b574464';
 
 const discoverMovie = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
-  // code here
-}
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`;
+  //same as axios.get('url')
+  axios(url)
+    .then(function (response) {
+    console.log(response);
+    });
+ return axios(url);  //to get jest to pass - equivalent of calling funct w/ statement; ?
+   
+};
+
+
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
-}
+  axios(url)
+    .then(function (response){
+    return result.data;
+  });
+};
+
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = '5783'
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
-  // code here
+  axios(url)
+    .then(function (response){
+      return status; 
+    })
+    .catch(function (error){
+      return error.response.status;
+    })
 }
-
 
 
 module.exports = {

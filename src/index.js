1d57564c809c9103117d86a9aea839666aa55749
Pena-Martinez <Rocio.PenaMartinez@austintexas.gov>
2021-05-15 17:24:42 -0500

    short => functs. all tests passed!

--- src/index.js
+++ src/index.js
@@ -7,31 +7,26 @@ const api_key = 'd771b19ef336ed8381def3a60b574464';
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`;
-  //same as axios.get('url')
-  axios(url)
-    .then(function (response) {
-    console.log(response);
-    });
+
+  axios(url)//same as axios.get('url')
+    .then( response => console.log(response));
  return axios(url);  //to get jest to pass - equivalent of calling funct w/ statement; ?
    
 };
 
 
-//returning undefined but request made it to endpoint ... how can i see results?
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`;
-  return axios(url).then(response => response.data);
-
-};
+  return axios(url)
+    .then( response => response.data);
 
 
 const getMovieByIdFailure = () => {
   const fakeId = '5783'
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`;
-  return axios(url).then(response => response) //same as response.status
-    .catch(function (error){
-      return error.response.status;
-    });
+  return axios(url)
+    .then( response => response) //same as response.data
+    .catch( error => error.response.status);
 };
 
 
